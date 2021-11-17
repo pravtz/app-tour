c588d7112569d88e6b13ec55f244e8b55db08e7a
pravtz <ederson.ufrgs@gmail.com>
2021-11-17 01:29:02 -0300

    add analytics in the project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   NextScript
 } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import Analytics from '../components/Analytics'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -40,6 +41,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     )
